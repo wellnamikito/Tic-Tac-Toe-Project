a7b437d2d354fc501d607f77af794a58d885ba19
Yqaronutymenarituslimanlio <deinwjncthawuinc@gmail.com>
2026-05-02 17:21:26 +0300

    Add README and update project config

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,17 +1,29 @@
-package org.example;
+package app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import javafx.application.Application;
+import javafx.stage.Stage;
+import view.MainMenuView;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        MainMenuView menu = new MainMenuView();
+
+        stage.setTitle("Tic Tac Toe");
+
+        stage.setWidth(1280);
+        stage.setHeight(720);
+
+        stage.setMinWidth(960);
+        stage.setMinHeight(540);
+
+        stage.setScene(menu.createScene(stage));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
